c984b1232d6ce2011cd29545dfe252f279104f24
AndrexHernandex <jahernandez6430@soy.sena.co>
2023-10-05 16:49:24 -0500

    version1

---
+++ Proyecto GA7-220501096-AA1-EV05/js/ejemplo.js
@@ -0,0 +1,20 @@
+var nombre = "Jorge"
+var numero1= 4
+var frutas = ["naranjas", "peras" , "manzanas"]
+var objetopersona = {
+    nombre: "Andres",
+    apellido: "Hernandez"
+}
+
+
+function mensaje() {
+    alert(nombre)
+    alert(numero1)
+    alert(frutas[1])
+    alert(objetopersona.apellido)
+
+
+}
+// document.write("Ejemplo de un texto")
+// alert("Esto es un ejemplo de una alerta")
+// console.log("Este es un ejemplo desde la consola desde otro archivo")
